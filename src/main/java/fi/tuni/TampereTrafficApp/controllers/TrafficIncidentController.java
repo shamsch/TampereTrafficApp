00b29d370ac166282d7ddc4dff4d1d1a3f0d6fce
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2024-10-20 00:20:59 +0300

    added incident view and incident details page
    Signed-off-by: Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>

--- src/main/java/fi/tuni/TampereTrafficApp/controllers/TrafficIncidentController.java
+++ src/main/java/fi/tuni/TampereTrafficApp/controllers/TrafficIncidentController.java
@@ -1,9 +1,36 @@
 package fi.tuni.TampereTrafficApp.controllers;
 
-/**
- *
- * @author Joel
- */
+import fi.tuni.TampereTrafficApp.models.TrafficIncident.SituationRecord;
+import fi.tuni.TampereTrafficApp.services.TrafficIncidentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/incident")
 public class TrafficIncidentController {
-    
+    private final TrafficIncidentService trafficIncidentService;
+
+    public TrafficIncidentController(TrafficIncidentService trafficIncidentService) {
+        this.trafficIncidentService = trafficIncidentService;
+    }
+
+    @GetMapping("/map")
+    public String showTrafficIncidents(Model model) {
+        List<SituationRecord> situationRecords = trafficIncidentService.getSituationRecords();
+        model.addAttribute("situationRecords", situationRecords);
+        return "incidentMapView";
+
+    }
+
+    @GetMapping("/details/{recordId}")
+    public String showTrafficIncidentDetails(@PathVariable String recordId, Model model) {
+        SituationRecord situationRecord = trafficIncidentService.getSituationRecordById(recordId).orElse(null);
+        model.addAttribute("situationRecord", situationRecord);
+        return "incidentDetailsView";
+    }
 }
