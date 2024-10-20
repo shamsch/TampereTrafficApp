a20a704ad64590e5341d1e44397d5ce569ba2650
Patrik Salmensaari <patrik.salmensaari@tuni.fi>
2024-10-20 21:19:35 +0300

    Changed to use lombok

--- src/main/java/fi/tuni/TampereTrafficApp/models/TrafficCamera/TrafficCamera.java
+++ src/main/java/fi/tuni/TampereTrafficApp/models/TrafficCamera/TrafficCamera.java
@@ -1,6 +1,10 @@
 package fi.tuni.TampereTrafficApp.models.TrafficCamera;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -9,41 +13,13 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TrafficCamera {
     private String cameraId;
     private String cameraName;
     private Location location;
     private List<CameraPreset> cameraPresets;
-
-    public String getCameraId() {
-        return cameraId;
-    }
-
-    public void setCameraId(String cameraId) {
-        this.cameraId = cameraId;
-    }
-
-    public String getCameraName() {
-        return cameraName;
-    }
-
-    public void setCameraName(String cameraName) {
-        this.cameraName = cameraName;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
-
-    public List<CameraPreset> getCameraPresets() {
-        return cameraPresets;
-    }
-
-    public void setCameraPresets(List<CameraPreset> cameraPresets) {
-        this.cameraPresets = cameraPresets;
-    }
 }
