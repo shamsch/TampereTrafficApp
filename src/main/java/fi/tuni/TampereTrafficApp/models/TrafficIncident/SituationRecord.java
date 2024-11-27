096dec67e91dae20552e9fa2af29d5074d441466
Joel Rauma <joel.rauma@tuni.fi>
2024-11-27 02:54:29 +0200

    Fixed ä ö å and time representations

--- src/main/java/fi/tuni/TampereTrafficApp/models/TrafficIncident/SituationRecord.java
+++ src/main/java/fi/tuni/TampereTrafficApp/models/TrafficIncident/SituationRecord.java
@@ -1,11 +1,17 @@
 package fi.tuni.TampereTrafficApp.models.TrafficIncident;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -32,4 +38,53 @@ public class SituationRecord {
     private Location location;
     @JsonProperty("temporaryLimitText")
     private List<String> temporaryLimitText;
+    
+    public SituationRecord(String id, String version, String creationTime, String versionTime, String startTime, String endTime, Type type, Type detailedType, String detailedTypeText, String severity, boolean safetyRelatedMessage, String sourceName, String generalPublicComment, String situationId, Location location, List<String> temporaryLimitText) {  
+        
+        System.out.println(creationTime);
+        
+        this.id = id;
+        this.version = version;
+        
+        this.creationTime = formatTimestamp(creationTime);
+        this.versionTime = formatTimestamp(versionTime);
+        this.startTime = formatTimestamp(startTime);
+        this.endTime = formatTimestamp(endTime); 
+        /*
+        this.creationTime = (creationTime);
+        this.versionTime = (versionTime);
+        this.startTime = (startTime);
+        this.endTime = (endTime);
+        */
+        this.type = type;
+        this.detailedType = detailedType;
+        this.detailedTypeText = detailedTypeText;
+        this.severity = severity.replaceAll("ï¿½", "ä");
+        this.safetyRelatedMessage = safetyRelatedMessage;
+        this.sourceName = sourceName.replaceAll("ï¿½", "ä");
+        this.generalPublicComment = generalPublicComment;
+        this.situationId = situationId;
+        this.location = location;
+        this.temporaryLimitText = temporaryLimitText;
+        
+    }
+
+    public static String formatTimestamp(String timestamp) {
+        if (timestamp != null) {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(timestamp.substring(0, 10)).append(" ");
+            Pattern timePattern = Pattern.compile("T(\\d{2}:\\d{2}:\\d{2})");
+            Matcher matcher = timePattern.matcher(timestamp);
+
+            if (matcher.find()) {
+                // Extract the matched time
+                sb.append(matcher.group(1).substring(0,5));
+            }
+
+            return sb.toString();
+        } else {
+            return "";
+        }
+    }
 }
